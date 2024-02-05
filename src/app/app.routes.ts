fb8199a8ef7bab60cd21df4897723e18f92e688a
AlmaIntanP <almaintan14@gmail.com>
2024-02-06 01:51:03 +0700

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AuthComponent
+    },
+    {
+        path: 'add',
+        data: { title: 'Add Employee' },
+        component: EmployeeComponent
+    }
+];
